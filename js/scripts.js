bd657ca550735080f193a3efb400d1b9bfd1ff75
Gpolly <gngugi17@gmail.com>
2019-12-17 07:52:21 +0300

    I have introduce background image on the location page

--- js/scripts.js
+++ js/scripts.js
@@ -232,14 +232,7 @@ $(function () {
 
     //Pick Up
     $(".delivernot").click(function () {
-        // $('#list').text(" ");     
-        // $("#list").append("<br>" + "Flavour :   " + newOrder.f + "<br>" + "Size :   "
-        //     + newOrder.s + "<br>" + "Crust :     "
-        //     + newOrder.c + "<br>" + "Toppings :     "
-        //     + newOrder.t + "<br>" + " Number of pizzas :    "
-        //     + newOrder.n + "<br>" + "Total Price :  "
-        //     + newOrder.total + "<br><br>").css('font-family', 'system-ui').css('font-size', '24px');
-   
+     
 
     });
 
